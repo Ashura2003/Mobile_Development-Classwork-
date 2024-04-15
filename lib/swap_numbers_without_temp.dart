795878609434f057451812c0f1709b7b1d191ce8
Ashura2003 <shreejanm21@gmail.com>
2024-04-15 13:17:39 +0545

    comments

--- lib/swap_numbers_without_temp.dart
+++ lib/swap_numbers_without_temp.dart
@@ -2,9 +2,9 @@ void swapNumber(int first, int second) {
   print("Before Swapping");
   print("First Number : $first \nSecond Number: $second");
 
-  first = first + second;
-  second = first - second;
-  first = first - second;
+  first = first + second; // 2 + 3 = 5
+  second = first - second; // 5 - 2 = 3
+  first = first - second; // 5 - 3 = 2
 
   print("Before Swapping");
   print("First Number : $first \nSecond Number: $second");
